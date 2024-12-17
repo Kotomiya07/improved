fda63e80e683429fff65cdadefbdc9039de94f27
Kotomiya07 <115519148+Kotomiya07@users.noreply.github.com>
2024-12-17 14:05:58 +0900

    Update test script for CIFAR-10: adjust epoch range to 1800-2000 and modify experiment name

--- test-cifar10-dit.sh
+++ test-cifar10-dit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for ((i=1200; i<=1700; i=i+5))
+for ((i=1800; i<=2000; i=i+5))
 do
-python3 test_dit_no_ddp.py --dataset cifar10 --exp cifar10-ori-dit-no-ddp-s2-skip-connection-predict-noise-plus2 --epoch_id $i --num_channels 4 \
+python3 test_dit_no_ddp.py --dataset cifar10 --exp cifar10-ori-dit-no-ddp-s2-skip-connection-predict-noise-plus-epoch2000 --epoch_id $i --num_channels 4 \
 --num_channels_dae 128 --num_timesteps 4 --num_res_blocks 2 --nz 50 --z_emb_dim 256 \
 --n_mlp 4 --ch_mult 1 2 2 --image_size 32 --current_resolution 16 --attn_resolutions 32 \
 --scale_factor 105.0 --AutoEncoder_config autoencoder/config/kl-f2.yaml --AutoEncoder_ckpt autoencoder/weight/kl-f2.ckpt \
