07dfd665be52834a6c6552ec307e477785d924bc
Kotomiya07 <115519148+Kotomiya07@users.noreply.github.com>
2025-01-14 02:23:33 +0900

    iddgan_run.shのcifar10-sn-asymmetric-5データセット用のトレーニングスクリプトで、エポック数を2000から10000に変更

--- iddgan_run.sh
+++ iddgan_run.sh
@@ -55,7 +55,7 @@ if [[ $MODE == train ]]; then
 	
 	elif [[ $DATASET == cifar10-sn-asymmetric-5  ]]; then
 		python3 train_iddgan_spectral_norm.py --dataset cifar10 --exp cifar10-spectralnorm-asymmetric-5 --num_channels 4 --num_channels_dae 128 --num_timesteps 4 \
-			--num_res_blocks 2 --batch_size 256 --num_epoch 2000 --ngf 64 --nz 50 --z_emb_dim 256 --n_mlp 4 --embedding_type positional \
+			--num_res_blocks 2 --batch_size 256 --num_epoch 10000 --ngf 64 --nz 50 --z_emb_dim 256 --n_mlp 4 --embedding_type positional \
 			--use_ema --ema_decay 0.9999 --r1_gamma 0.02 --lr_d 1.25e-4 --lr_g 1.6e-4 --lazy_reg 15 \
 			--ch_mult 1 2 2 --save_content --datadir ./data/cifar-10 \
 			--master_port $MASTER_PORT --num_process_per_node $GPUS --save_ckpt_every 5 \
